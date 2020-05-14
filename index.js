78149ff06b88039ac6847ca064f6c31289ef0e9d
Olumide <volumide42@gmai.com>
2020-05-14 13:19:16 +0100

    finish ui

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let vid = document.getElementById('video')
 let currentTime = document.getElementById('current-time')
+let heartOutline = document.getElementById('heart-outline')
+let heart = document.getElementById('heart')
 let duration = 0
 let sec = 0
 let min = 0
@@ -8,17 +10,37 @@ let min = 0
 vid.addEventListener('click', () => {
 	if (vid.paused || vid.ended) vid.play()
 	else vid.pause()
-
-
-
 })
+
+let toggle = () =>{
+	return setTimeout(() => {
+		heartOutline.classList.toggle('hide')
+		heart.classList.toggle('hide')
+	}, 500);
+}
+
+function like(){
+	console.log("liked")
+	toggle()
+	
+}
+
+function unlike(){
+	console.log("disliked")
+	toggle()
+}
+// function unlike(){
+// 	heartOutline.classList.add('hide')
+// 	heart.classList.remove('hide')
+// 	heart.classList.add('show')
+// }
+
 
 // console.log(vid.duration)
 let set = () =>{
 	setInterval(() => {
 		
 		let time = Math.floor(vid.currentTime)
-		console.log(time % 60)
 	
 		let tesMin = time % 60
 
@@ -27,7 +49,6 @@ let set = () =>{
 		}
 		let hour = Math.floor(time/60);
 		let remainTime = Math.floor(vid.duration - time)
-		console.log(time, remainTime)
 
 		if(min < 10)
 			if (hour === 0) 
